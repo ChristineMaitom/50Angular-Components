d6754ba7c55203e17b0002856fbee9096ca82885
jane <janemaitom@gmail.com>
2024-09-25 21:28:03 +0800

    #21 add temperatureconverter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import {FahrenheittocelsiusComponent} from "./components/fahrenheittocelsius/fah
 import {BookmarklistComponent} from "./components/bookmarklist/bookmarklist.component";
 import {CharactercounterComponent} from "./components/charactercounter/charactercounter.component";
 import {PalindromecheckerComponent} from "./components/palindromechecker/palindromechecker.component";
+import {TemperatureConverterComponent} from "./components/temperatureconverter/temperatureconverter.component";
 
 
 const routes: Routes = [
@@ -48,7 +49,7 @@ const routes: Routes = [
   {path: 'bookmarklist',  component:BookmarklistComponent},
   {path: 'charactercounter',  component:CharactercounterComponent},
   {path: 'palindromechecker',  component:PalindromecheckerComponent},
-
+  {path: 'temperatureconverter',  component:TemperatureConverterComponent},
 
 ]
 
@@ -82,6 +83,7 @@ const routes: Routes = [
     BookmarklistComponent,
     CharactercounterComponent,
     PalindromecheckerComponent,
+    TemperatureConverterComponent
 
   ],
 
