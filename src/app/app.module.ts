d16f3c5278f8f7548595caaca80f789c1d05614a
jane <janemaitom@gmail.com>
2024-09-26 17:05:12 +0800

    #39 add uppercasegreeting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import {FibonacciGeneratorComponent} from "./components/fibonaccigenerator/fibon
 import {OddSumCalculatorComponent} from "./components/oddsumcalculator/oddsumcalculator.component";
 import {CurrencyFormatterComponent} from "./components/currencyformatter/currencyformatter.component";
 import {RandomQuoteDisplayComponent} from "./components/randomquotedisplay/randomquotedisplay.component";
+import {UppercaseGreetingComponent} from "./components/uppercasegreeting/uppercasegreeting.component";
 
 
 const routes: Routes = [
@@ -84,6 +85,8 @@ const routes: Routes = [
   {path: 'oddsumcalculator',  component:OddSumCalculatorComponent},
   {path: 'currencyformatter',  component:CurrencyFormatterComponent},
   {path: 'randomquotedisplay',  component:RandomQuoteDisplayComponent},
+  {path: 'uppercasegreeting',  component:UppercaseGreetingComponent},
+
 
 
 ]
@@ -136,6 +139,7 @@ const routes: Routes = [
     OddSumCalculatorComponent,
     CurrencyFormatterComponent,
     RandomQuoteDisplayComponent,
+    UppercaseGreetingComponent,
 
 
   ],
